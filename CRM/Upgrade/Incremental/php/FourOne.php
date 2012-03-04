73e3c263ad3a96f7eb5031714d1ed3ca266fb87a
Mark Muhleder <mark@analogweb.co.uk>
2012-03-04 02:12:34 +0000

    SQL upgrade scripts
    Signed-off-by: Mark Muhleder <mark@analogweb.co.uk>

--- CRM/Upgrade/Incremental/php/FourOne.php
+++ CRM/Upgrade/Incremental/php/FourOne.php
@@ -393,6 +393,19 @@ AND    v.is_active = 1
         $upgrade->processSQL( $rev );
     }
 
+    function upgrade_4_1_CRM9587($rev) {
+        // Create an event registration profile with a single email field
+        $sql = "INSERT INTO `civicrm_uf_group` (`is_active`, `group_type`, `title`, `help_pre`, `help_post`, `limit_listings_group_id`, `post_URL`, `add_to_group_id`, `add_captcha`, `is_map`, `is_edit_link`, `is_uf_link`, `is_update_dupe`, `cancel_URL`, `is_cms_user`, `notify`, `is_reserved`, `name`, `created_id`, `created_date`, `is_proximity_search`)
+                VALUES (1, 'Individual, Contact', 'Event Registration', NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 0, NULL, 0, NULL, 0, 'event_registration', NULL, NULL, 0);";
+        $dao = CRM_Core_DAO::executeQuery($sql);
+        $sql = "INSERT INTO `civicrm_uf_field` (`uf_group_id`, `field_name`, `is_active`, `is_view`, `is_required`, `weight`, `help_post`, `help_pre`, `visibility`, `in_selector`, `is_searchable`, `location_type_id`, `phone_type_id`, `label`, `field_type`, `is_reserved`)
+                VALUES ({$dao->id}, 'email', 1, 0, 1, 1, NULL, NULL, 'User and User Admin Only', 0, 0, NULL, NULL, 'Email Address', 'Contact', 0);";
+        CRM_Core_DAO::executeQuery($sql);
+
+        $upgrade = new CRM_Upgrade_Form();
+        $upgrade->processSQL($rev);
+    }
+
     function getTemplateMessage( ) {
         return "Blah";
     }
